fc85938ebcaa45955e9067527ed7490f10e686f6
psychokiller2144 <79776630+psychokiller2144@users.noreply.github.com>
2022-10-10 02:40:31 -0500

    Add files via upload

---
+++ Lesson7/SgaWebHibernateJPA/src/main/java/mx/com/gm/test/cascade/PersistenciaCascadaTest.java
@@ -0,0 +1,37 @@
+package mx.com.gm.test.cascade;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import mx.com.gm.domain.Alumno;
+import mx.com.gm.domain.Contacto;
+import mx.com.gm.domain.Domicilio;
+
+public class PersistenciaCascadaTest {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateJpaPU");
+        EntityManager em = emf.createEntityManager();
+        
+        Domicilio domicilio = new Domicilio();
+        domicilio.setCalle("Nogales");
+        domicilio.setNoCalle("10");
+        domicilio.setPais("Mexico");
+        
+        Contacto contacto = new Contacto();
+        contacto.setEmail("clara@mail.com");
+        contacto.setTelefono("44332211");
+
+        Alumno alumno = new Alumno();
+        alumno.setNombre("Carlos");
+        alumno.setApellido("Lara");
+        alumno.setDomicilio(domicilio);
+        alumno.setContacto(contacto);
+        
+        em.getTransaction().begin();
+        em.persist(alumno);
+        em.getTransaction().commit();
+        
+        System.out.println("alumno = " + alumno);
+    }
+}
---
+++ Lesson7/SgaWebHibernateJPA/src/main/java/mx/com/gm/web/ServletAgregar.java
@@ -0,0 +1,47 @@
+package mx.com.gm.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import mx.com.gm.domain.Alumno;
+import mx.com.gm.domain.Contacto;
+import mx.com.gm.domain.Domicilio;
+import mx.com.gm.servicio.ServicioAlumno;
+
+@WebServlet("/ServletAgregar")
+public class ServletAgregar extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException{
+        
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String calle = request.getParameter("calle");
+        String noCalle = request.getParameter("noCalle");
+        String pais = request.getParameter("pais");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        
+        Domicilio domicilio = new Domicilio();
+        domicilio.setCalle(calle);
+        domicilio.setNoCalle(noCalle);
+        domicilio.setPais(pais);
+        
+        Contacto contacto = new Contacto();
+        contacto.setEmail(email);
+        contacto.setTelefono(telefono);
+        
+        Alumno alumno = new Alumno();
+        alumno.setNombre(nombre);
+        alumno.setApellido(apellido);
+        alumno.setDomicilio(domicilio);
+        alumno.setContacto(contacto);
+        
+        ServicioAlumno servicioAlumno = new ServicioAlumno();
+        servicioAlumno.guardarAlumno(alumno);
+        
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
